d788ed6fe608ce668a09e92e233673833e29430e
José Alcántara <jose.alcantaramunoz@alum.uca.es>
2014-05-31 08:53:10 +0200

    Cambios para arreglar la función calculate_result

--- pb.c
+++ pb.c
@@ -47,27 +47,24 @@ static int my_sort_func(const void* p1, const void* p2)
 }
 
 
-int calculate_result(int white_balls[5], int power_ball, int *lott)
+float calculate_result(int white_balls[5], int power_ball, int *lott)
 {
     float resultado = 0;
     
     if(power_ball == lott[5])
-        resultado += 0.1
+        resultado += 0.1;
     
     for (int i=0; i<5; i++) {
         if ((white_balls[i] < 1) || (white_balls[i] > MAX_WHITE_BALL)) {
             return -1;
         }
 	    
-	    if(white_balls[i] != lott[i])
-	        resultado += 0.2
+	    if(white_balls[i] == lott[i])
+	        resultado += 0.2;
 	     
     }
   
-  // lottery ball numbers are always shown sorted
-  
-  
-  return 0;
+    return resultado;
 }
 
 void showing_results(int white_balls[5], int power_ball)
@@ -140,7 +137,7 @@ int main(int argc, char** argv)
     
     printf("\n--- The lottery numbers ---\n");
     lottery_numbers_simulation(lott);
-    int result = calculate_result(balls, power_ball, lott);
+    float result = calculate_result(balls, power_ball, lott);
     showing_results(balls, power_ball);
 
     if (result <0)
@@ -166,7 +163,7 @@ int main(int argc, char** argv)
     
     printf("\n--- The lottery numbers ---\n");
     lottery_numbers_simulation(lott);
-    printf("%d percent chance of winning\n", result);
+    printf("%f percent chance of winning\n", result*100);
 
     return 0;
     
